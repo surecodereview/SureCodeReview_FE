b205dc095e86a9f0e237f59bfdec2eb85ee0e555
nullyng <nullyng@gmail.com>
2024-12-16 16:26:47 +0900

    chore: api 절대경로 및 proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,18 @@ export default defineConfig({
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@api', replacement: path.resolve(__dirname, 'src/api/') },
       { find: '@assets', replacement: path.resolve(__dirname, 'public/assets/') },
     ]
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   define: {
     'process.env': {}
