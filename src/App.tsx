8fd1839cba562fbfaace8b00344ce41562cfe556
nullyng <nullyng@gmail.com>
2024-12-18 13:24:46 +0900

    feat: 로컬, 원격 선택 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { globalStyles } from "./globalStyles"
 import ReviewPage from "@/pages/ReviewPage"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { RecoilRoot } from "recoil"
+import ChoosePage from "./pages/ChoosePage"
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Layout>
           <Global styles={globalStyles} />
           <Routes>
-            <Route path="/" element={<SettingPage />} />
+            <Route path="/" element={<ChoosePage />} />
+            <Route path="/setting" element={<SettingPage />} />
             <Route path="/review" element={<ReviewPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
---
+++ src/pages/ChoosePage.tsx
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+import Title from "@/components/common/Title";
+import { Container } from "@/components/common/Container";
+import { useNavigate } from "react-router-dom";
+import Button from "@/components/common/Button";
+import { useSetRecoilState } from "recoil";
+import { Target, targetState } from "@/recoil/atoms/targetState";
+
+const ChoosePage = () => {
+    const navigate = useNavigate();
+    const setTarget = useSetRecoilState(targetState);
+
+    const handleButtonClick = (target: Target) => {
+        setTarget(target);
+        navigate("/setting");
+        console.log(target)
+    }
+
+    return <>
+        <Container>
+            <Title />
+            <Content>
+                <Button onClick={() => handleButtonClick("local")}>
+                    로컬 저장소<br />코드 분석하기
+                </Button>
+                <Button onClick={() => handleButtonClick("remote")}>
+                    원격 저장소<br />코드 분석하기
+                </Button>
+            </Content>
+
+        </Container>
+    </>
+}
+
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    gap: 12px;
+    justify-content: center;
+    padding: 0 2rem;
+    padding-bottom: 5rem;
+`
+
+export default ChoosePage;
