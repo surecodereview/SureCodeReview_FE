d12f0cd9d4bdc8bb7663ad52e55a8e7a3d4b0fa9
nullyng <nullyng@gmail.com>
2024-12-18 15:28:01 +0900

    feat: 원격 저장소 commit에 대한 타입 추가

--- src/recoil/atoms/commitState.ts
+++ src/recoil/atoms/commitState.ts
@@ -1,11 +1,18 @@
 import { atom } from "recoil";
 
-interface Commit {
-    commitId: string;
-    commitMessage: string;
-}
-
 export const commitState = atom<Commit[]>({
     key: "commitState",
     default: []
 })
+
+export interface Commit {
+    commitId: string;
+    commitMessage: string;
+}
+
+export interface RemoteCommit {
+    author: string;
+    date: string;
+    message: string;
+    sha: string;
+};
